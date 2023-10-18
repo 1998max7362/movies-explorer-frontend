62e78784e962405b4a19d07e7de78052ba2e34f4
Max <1998max7362@mail.ru>
2023-10-18 12:44:06 +0300

    На страницах с фильмами теперь выделяется текущая страница в хедере

--- src/components/Header/HeaderLoggedIn.js
+++ src/components/Header/HeaderLoggedIn.js
@@ -12,8 +12,8 @@ export const HeaderLoggedIn = ({ pathname, openMenu,windowSize }) => {
       </>
       : <>
         <div className={`header__navigation-container ${pathname === '/' && 'header__navigation-container_main'}`}>
-          <Link className={`link ${pathname === '/' && 'header__link-ismain'}`} to="/movies">Фильмы</Link>
-          <Link className={`link ${pathname === '/' && 'header__link-ismain'}`} to="/saved-movies">Сохранённые фильмы</Link>
+          <Link className={`link ${pathname === '/movies' && 'header__link_selected'}`} to="/movies">Фильмы</Link>
+          <Link className={`link ${pathname === '/saved-movies' && 'header__link_selected'}`} to="/saved-movies">Сохранённые фильмы</Link>
         </div>
         <Link className={`profile-button link ${pathname === '/' && 'header__profile-main'}`} to="/profile">
           <p className="text">Аккаунт</p>
