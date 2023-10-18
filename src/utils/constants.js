1b95d78f32964586deebe3c03e2607e398a4a207
Max <1998max7362@mail.ru>
2023-10-18 04:57:16 +0300

    заливаю проект с билдом тк не хочет билдится на сервер

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const url = 'https://nobodymaximdiploma.api.nomoredomainsrocks.ru/'
+export const url = 'https://nobodymaximdiploma.api.nomoredomainsrocks.ru'
 export const filmUrl = 'https://api.nomoreparties.co'
