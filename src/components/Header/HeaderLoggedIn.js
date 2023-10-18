71af8a1151eb19cc1c1b5c187a7314a133f1f3e3
Max <1998max7362@mail.ru>
2023-10-18 13:47:18 +0300

    Цвета слов в хедере поменял

--- src/components/Header/HeaderLoggedIn.js
+++ src/components/Header/HeaderLoggedIn.js
@@ -1,24 +1,59 @@
-import headerProfile from "../../images/Header/header-profile.svg"
-import navButton from "../../images/Header/nav-button.svg"
-import navButtonLight from "../../images/Header/nav-button-white.svg"
+import headerProfile from '../../images/Header/header-profile.svg';
+import navButton from '../../images/Header/nav-button.svg';
+import navButtonLight from '../../images/Header/nav-button-white.svg';
 import { Link } from 'react-router-dom';
 
-export const HeaderLoggedIn = ({ pathname, openMenu,windowSize }) => {
-
+export const HeaderLoggedIn = ({ pathname, openMenu, windowSize }) => {
   return (
-    <>{windowSize <= 768 ?
-      <>
-        <img className="link header__nav-button" src={pathname === '/' ? navButtonLight : navButton} alt="Кнопка навигации" onClick={openMenu} />
-      </>
-      : <>
-        <div className={`header__navigation-container ${pathname === '/' && 'header__navigation-container_main'}`}>
-          <Link className={`link ${pathname === '/movies' && 'header__link_selected'}`} to="/movies">Фильмы</Link>
-          <Link className={`link ${pathname === '/saved-movies' && 'header__link_selected'}`} to="/saved-movies">Сохранённые фильмы</Link>
-        </div>
-        <Link className={`profile-button link ${pathname === '/' && 'header__profile-main'}`} to="/profile">
-          <p className="text">Аккаунт</p>
-          <img className="profile-button-logo" src={headerProfile} alt="Лого-аккаунт" />
-        </Link>
-      </>}
-    </>)
-} 
+    <>
+      {windowSize <= 768 ? (
+        <>
+          <img
+            className='link header__nav-button'
+            src={pathname === '/' ? navButtonLight : navButton}
+            alt='Кнопка навигации'
+            onClick={openMenu}
+          />
+        </>
+      ) : (
+        <>
+          <div
+            className={`header__navigation-container ${
+              pathname === '/' && 'header__navigation-container_main'
+            }`}
+          >
+            <Link
+              className={`link ${pathname === '/' && 'header__link-ismain'} ${
+                pathname === '/movies' && 'header__link_selected'
+              }`}
+              to='/movies'
+            >
+              Фильмы
+            </Link>
+            <Link
+              className={`link ${pathname === '/' && 'header__link-ismain'} ${
+                pathname === '/saved-movies' && 'header__link_selected'
+              }`}
+              to='/saved-movies'
+            >
+              Сохранённые фильмы
+            </Link>
+          </div>
+          <Link
+            className={`profile-button link ${
+              pathname === '/' && 'header__profile-main'
+            }`}
+            to='/profile'
+          >
+            <p className='text'>Аккаунт</p>
+            <img
+              className='profile-button-logo'
+              src={headerProfile}
+              alt='Лого-аккаунт'
+            />
+          </Link>
+        </>
+      )}
+    </>
+  );
+};
